3fef3c351c4780806ac36d86023018fe530895f8
xcus33me <HarvieOrigin@yandex.ru>
2024-01-16 18:03:09 +0300

    Added initWindow() realization; Added some class fields;

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <stdexcept>
 #include <cstdlib>
 
-class  {
+class MyApplication{
 public:
     void run() {
         initWindow();
@@ -17,15 +17,19 @@ public:
     }
 
 private:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+    GLFWwindow* window;
+
+
     void initWindow() {
-        // GLFW INITIALIZE
         glfwInit();
 
         // We need to tell it to not create an OpenGL context with a subsequent call:
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Temporarily
 
+        window = glfwCreateWindow(WIDTH, HEIGHT, "ShaderPractice", nullptr, nullptr);
     }
 
     void initVulkan() {
@@ -33,17 +37,20 @@ private:
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
-
 };
 
 int main() {
-    HelloTriangleApplication app;
+    MyApplication app;
 
     try {
         app.run();
